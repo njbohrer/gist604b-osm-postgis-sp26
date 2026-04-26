0ad3b3d2a7f4417b5ce4ba0b3ae9b5b7dbe90386
Noah Bohrer <njbohrer@gmail.com>
2026-04-26 16:17:30 +0000

    updated after reloading area shp files into database

---
+++ sql/alaska/02_farm_area_by_borough.sql
@@ -0,0 +1,19 @@
+--Taking the Borough (Alaska doesn't have counties) level across Alaska and calculating the sq km of farm area for each
+
+SELECT
+    aa.name AS borough_name,
+    SUM(ST_Area(ST_Intersection(l.geom, aa.geom)::geography)) / 1000000 AS farmland_area_sq_km,
+    aa.geom
+FROM
+    adminareas_a AS aa
+JOIN
+    landuse_a AS l
+    ON ST_Intersects(aa.geom, l.geom)
+WHERE
+    aa.fclass = 'admin_level6'
+    AND l.fclass = 'farmland'
+    AND aa.name IS NOT NULL
+GROUP BY
+    aa.name, aa.geom
+ORDER BY
+    farmland_area_sq_km DESC;
